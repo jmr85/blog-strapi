4725c94e134a8180fc8eede769491657d1951701
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2022-03-13 09:35:10 -0300

    first commit

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'blog-strapi.wxn1w.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'blog-strapi'),
+        username: env('DATABASE_USERNAME', 'admin-strapi'),
+        password: env('DATABASE_PASSWORD', 'admin-strapi-123456'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f9e01c15-8a70-48d8-b39e-be66c118afbe'
+};
